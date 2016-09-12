f3600736c7f67311be8e8574c2e6e494d6a742c8
Jonas De Beukelaer <jonas.debeukelaer@underwriteme.co.uk>
2016-09-12 09:45:12 +0100

    trying to fix express.static issue

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.normalize(path.join(__dirname, '/public'))));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -61,4 +61,4 @@ module.exports = app;
 
 app.listen(process.env.PORT || 3333);
 
-console.log('Started server on port ' + process.env.PORT || 3333);
+console.log('Started server on port ' + (process.env.PORT || 3333));
